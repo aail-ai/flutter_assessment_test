40c410953aaa039ee023971ba434d1977d199866
Gilang Liberty <gilangliberty05@gmail.com>
2024-12-07 18:41:07 +0700

    feat: init flutter project

---
+++ lib/modules/chat/domain/entities/chat_message.dart
@@ -0,0 +1,11 @@
+import 'package:equatable/equatable.dart';
+
+class ChatMessage extends Equatable {
+  final String text;
+  final bool isUser;
+
+  const ChatMessage({required this.text, required this.isUser});
+
+  @override
+  List<Object?> get props => [text, isUser];
+}
