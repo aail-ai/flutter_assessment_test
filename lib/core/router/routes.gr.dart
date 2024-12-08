27312edbb070af56869a3ebe4112f88d27c6920d
Gilang Liberty <gilangliberty05@gmail.com>
2024-12-09 00:30:14 +0700

    feat: update ChatRoute to support initial data and enhance argument handling

--- lib/core/router/routes.gr.dart
+++ lib/core/router/routes.gr.dart
@@ -9,6 +9,9 @@
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:auto_route/auto_route.dart' as _i3;
+import 'package:flutter/material.dart' as _i4;
+import 'package:flutter_assessment_test/modules/chat/domain/entities/chat_message_histories.dart'
+    as _i5;
 import 'package:flutter_assessment_test/modules/chat/presentation/chat_message_screen.dart'
     as _i1;
 import 'package:flutter_assessment_test/modules/home/presentation/home_screen.dart'
@@ -16,10 +19,17 @@ import 'package:flutter_assessment_test/modules/home/presentation/home_screen.da
 
 /// generated route for
 /// [_i1.ChatScreen]
-class ChatRoute extends _i3.PageRouteInfo<void> {
-  const ChatRoute({List<_i3.PageRouteInfo>? children})
-      : super(
+class ChatRoute extends _i3.PageRouteInfo<ChatRouteArgs> {
+  ChatRoute({
+    _i4.Key? key,
+    required _i5.ChatMessageHistories? initialData,
+    List<_i3.PageRouteInfo>? children,
+  }) : super(
           ChatRoute.name,
+          args: ChatRouteArgs(
+            key: key,
+            initialData: initialData,
+          ),
           initialChildren: children,
         );
 
@@ -28,9 +38,29 @@ class ChatRoute extends _i3.PageRouteInfo<void> {
   static _i3.PageInfo page = _i3.PageInfo(
     name,
     builder: (data) {
-      return const _i1.ChatScreen();
+      final args = data.argsAs<ChatRouteArgs>();
+      return _i1.ChatScreen(
+        key: args.key,
+        initialData: args.initialData,
+      );
     },
   );
+}
+
+class ChatRouteArgs {
+  const ChatRouteArgs({
+    this.key,
+    required this.initialData,
+  });
+
+  final _i4.Key? key;
+
+  final _i5.ChatMessageHistories? initialData;
+
+  @override
+  String toString() {
+    return 'ChatRouteArgs{key: $key, initialData: $initialData}';
+  }
 }
 
 /// generated route for
