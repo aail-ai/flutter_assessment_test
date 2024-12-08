5446b64adcafab4b654d0c93dd365a783a5e77b4
Gilang Liberty <gilangliberty05@gmail.com>
2024-12-09 00:28:56 +0700

    feat: enhance JSON serialization for ChatMessage and ChatMessageHistories

--- lib/modules/chat/domain/entities/chat_message.dart
+++ lib/modules/chat/domain/entities/chat_message.dart
@@ -5,20 +5,28 @@ class ChatMessage extends Equatable {
   final bool isUser;
   final DateTime timestamp;
 
-  const ChatMessage({required this.text, required this.isUser, required this.timestamp});
+  const ChatMessage({
+    required this.text,
+    required this.isUser,
+    required this.timestamp,
+  });
+
+  factory ChatMessage.fromJson(Map<String, dynamic> json) {
+    return ChatMessage(
+      text: json['text'] as String,
+      isUser: json['isUser'] as bool,
+      timestamp: DateTime.fromMillisecondsSinceEpoch(json['timestamp'] as int),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'text': text,
+      'isUser': isUser,
+      'timestamp': timestamp.millisecondsSinceEpoch,
+    };
+  }
 
   @override
-  List<Object?> get props => [text, isUser];
-
-  factory ChatMessage.fromJson(Map<String, dynamic> json) => ChatMessage(
-        text: json['text'],
-        isUser: json['isUser'],
-        timestamp: DateTime.fromMillisecondsSinceEpoch(json['timestamp']),
-      );
-
-  Map<String, dynamic> toJson() => {
-        'text': text,
-        'isUser': isUser,
-        'timestamp': timestamp.millisecondsSinceEpoch,
-      };
+  List<Object?> get props => [text, isUser, timestamp];
 }
--- lib/modules/chat/domain/entities/chat_message_histories.dart
+++ lib/modules/chat/domain/entities/chat_message_histories.dart
@@ -5,18 +5,27 @@ class ChatMessageHistories extends Equatable {
   final List<ChatMessage> chatMessages;
   final DateTime timestamp;
 
-  const ChatMessageHistories({required this.chatMessages, required this.timestamp});
+  const ChatMessageHistories({
+    required this.chatMessages,
+    required this.timestamp,
+  });
+
+  factory ChatMessageHistories.fromJson(Map<String, dynamic> json) {
+    return ChatMessageHistories(
+      chatMessages: (json['chatMessages'] as List<dynamic>)
+          .map((e) => ChatMessage.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      timestamp: DateTime.fromMillisecondsSinceEpoch(json['timestamp'] as int),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'chatMessages': chatMessages.map((e) => e.toJson()).toList(),
+      'timestamp': timestamp.millisecondsSinceEpoch,
+    };
+  }
 
   @override
   List<Object?> get props => [chatMessages, timestamp];
-
-  factory ChatMessageHistories.fromJson(Map<String, dynamic> json) => ChatMessageHistories(
-        chatMessages: json['chatMessages'].map((msg) => ChatMessage.fromJson(msg)).toList(),
-        timestamp: DateTime.fromMillisecondsSinceEpoch(json['timestamp']),
-      );
-
-  Map<String, dynamic> toJson() => {
-        'chatMessages': chatMessages.map((msg) => msg.toJson()).toList(),
-        'timestamp': timestamp.millisecondsSinceEpoch,
-      };  
 }
